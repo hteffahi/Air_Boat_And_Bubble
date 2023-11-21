8c826897304cc4d8bee973fd064f5ae9b9ae9430
Flavien Leray <lerayflavien@gmail.com>
2023-11-21 12:47:13 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :boats do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: %i[:new, :create, :edit, :update]
   end
 end
